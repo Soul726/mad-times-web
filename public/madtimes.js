f25b3e5945ecfa93ea6b1c1700ccb1eeef94fdbd
Soul726 <151096943+Soul726@users.noreply.github.com>
2025-06-18 09:40:12 +0300

    Update madtimes.js

--- public/madtimes.js
+++ public/madtimes.js
@@ -1,1 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const div = document.createElement('div');
+  div.innerHTML = `<h2 style="color:lime;">✅ Hello from MAD TIMES JS</h2>`;
+  document.body.appendChild(div);
+});
 
